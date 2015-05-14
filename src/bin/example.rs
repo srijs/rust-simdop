8bba196d4111f563dd8c3d0f029a034dc1fcd01a
Sam Rijs <srijs@airpost.net>
2015-05-14 20:56:42 +1000

    refactor: use Option::map in example

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -7,20 +7,20 @@ use simdop::*;
 
 fn main() {
 
-  let sse2 = arch::x86::SSE2::detect().unwrap();
-
   let m: M4<i32> = mvec![1,2,3,4];
   println!("M: {:?}", m);
 
-  let m1: M2<i64> = sse2.set1(1);
-  let m2: M2<i64> = sse2.set1(2);
-  println!("Sum:    {:?}", sse2.add(m1, m2));
-  println!("Double: {:?}", sse2.shli(m1, 1));
-  println!("Half:   {:?}", sse2.shri(m2, 1));
-  let m3: M4<i32> = sse2.set1(1);
-  let m4: M4<i32> = sse2.set1(2);
-  println!("Sum:    {:?}", sse2.add(m3, m4));
-  println!("Double: {:?}", sse2.shli(m3, 1));
-  println!("Half:   {:?}", sse2.shri(m4, 1));
+  arch::x86::SSE2::detect().map(|sse2| {
+    let m1: M2<i64> = sse2.set1(1);
+    let m2: M2<i64> = sse2.set1(2);
+    println!("Sum:    {:?}", sse2.add(m1, m2));
+    println!("Double: {:?}", sse2.shli(m1, 1));
+    println!("Half:   {:?}", sse2.shri(m2, 1));
+    let m3: M4<i32> = sse2.set1(1);
+    let m4: M4<i32> = sse2.set1(2);
+    println!("Sum:    {:?}", sse2.add(m3, m4));
+    println!("Double: {:?}", sse2.shli(m3, 1));
+    println!("Half:   {:?}", sse2.shri(m4, 1));
+  });
 
 }
